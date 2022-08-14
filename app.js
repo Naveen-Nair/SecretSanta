0704664bdeaaebd60b75fbfd8410a28b1f798657
Naveen-Nair <naveensnair@outlook.com>
2022-08-14 21:29:50 +0530

    Optimised code for createGroups and Added code for JoinGroups

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post("/register",(req,res)=>{
           password:password
         })
         user.save()
-        currentUserId = user.__id;
+        currentUserId = user._id;
         res.sendFile(__dirname+'/htmlFiles/secretsanta/start.html') //go to the start of the page
 
       }
@@ -82,7 +82,7 @@ app.post('/login',(req,res)=>{
         if(user.password === password){//if the hashed password match
 
           console.log("user authenticated")
-          currentUserId = user.__id;
+          currentUserId = user._id;
           res.sendFile(__dirname+'/htmlFiles/secretsanta/start.html') //go to the start of the page
 
         }else{//if password fails
@@ -110,22 +110,38 @@ const groupSchema = new mongoose.Schema({
   budget:Number,
   location:String,
 
-  users: userSchema
+  requestid:String,
+  users:[{user:String}]
 
 })
 
 const Group = mongoose.model('Group',groupSchema)
 
 
+//******************************************************************************************
+//get and post request to start.html
+app.get('/start',(req,res)=>{
+  console.log(currentUserId)
+
+  if(currentUserId===''){//check if the user authenticated before
+    console.log('unauthenticated user tried to use')
+    //if not redirected to the login page
+    res.redirect('/login')
+
+  }else{
+    res.sendFile(__dirname + '/htmlFiles/secretsanta/start.html')
+
+  }
+})
 
 
 //******************************************************************************************
 //get and post request to createGroup
 app.get('/createGroup',(req,res)=>{
   if(currentUserId===''){//check if the user authenticated before
-    console.log('unauthenticated user tried to log in to create Group')
+    console.log('unauthenticated user tried to use')
     //if not redirected to the login page
-      res.redirect('/login')
+    res.redirect('/login')
 
   }else{
     //if the user is authenticated, and the button is pressed from the html
@@ -141,6 +157,14 @@ app.post('/createGroup',(req,res)=>{
   const budget= req.body.budget
   const location= req.body.Location
 
+
+  //creation of the unique id (to request)
+  const groupid = md5(name)
+  const len = groupid.length
+  //take the last 8 digits of the hashed name of the group
+  const uniqueid = groupid.substring(len-8,len)
+
+
   Group.findOne({name:name},(err,group)=>{
     if(err){
       console.log(err)
@@ -148,12 +172,7 @@ app.post('/createGroup',(req,res)=>{
       if(group){//if group exists
         res.send('<div>Group name already exists! try again</div>'+'<a href="/createGroup">Go back</a>')
       }else{
-
-        User.findOne({__id:currentUserId},(err,user)=>{
-          if(err){
-            console.log(err)
-          }else{
-            if(user){//if the current user is found
+        console.log(currentUserId)
 
               const group = new Group({
                 name:name,
@@ -161,28 +180,80 @@ app.post('/createGroup',(req,res)=>{
                 regisDate:regisDate,
                 budget:budget,
                 location:location,
-                users: user
+                requestid:uniqueid,
               })
+              group.users.push(currentUserId)
 
               group.save()
-
               res.redirect('/yourGroup')
 
 
-            }
-            else{//if the current group is
-              res.send('Error')
-            }
           }
-        })
+        }
 
 
+
+    })
+})
+
+
+
+//******************************************************************************************
+//get and post request to joinGroup
+app.get('/joinGroup',(req,res)=>{
+  console.log(currentUserId)
+
+  if(currentUserId===''){//check if the user authenticated before
+    console.log('unauthenticated user tried to use')
+    //if not redirected to the login page
+    res.redirect('/login')
+
+  }else{
+    res.sendFile(__dirname + '/htmlFiles/secretsanta/joinGroup.html')
+
+  }
+})
+
+app.post('/joinGroup',(req,res)=>{
+    const uniqueid = req.body.uniqueid;
+
+    Group.findOne({requestid:uniqueid},(err,group)=>{
+      if(err){
+        console.log(err)
+      }else{
+        if(group){
+          console.log(currentUserId)
+          group.users.push(currentUserId)
+          group.save()
+
+          res.send('done')
+
+        }else{
+          res.send('No such group')
+        }
       }
-    }
-  })
-
+    })
 
 })
+
+
+
+
+//******************************************************************************************
+//get and post request to yourGroup
+app.get('/yourGroup',(req,res)=>{
+  if(currentUserId===''){//check if the user authenticated before
+    console.log('unauthenticated user tried to use')
+    //if not redirected to the login page
+    res.redirect('/login')
+
+  }else{
+    res.sendFile(__dirname + '/htmlFiles/secretsanta/yourGroup.html')
+  }
+})
+
+
+
 
 
 
