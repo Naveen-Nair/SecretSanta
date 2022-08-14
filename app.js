639b5b581562be2d02000c0ddfae18fbabb09802
Naveen-Nair <naveensnair@outlook.com>
2022-08-14 16:37:57 +0530

    optimised the code for registeration, and also added code for login

--- app.js
+++ app.js
@@ -33,36 +33,52 @@ app.post("/register",(req,res)=>{
   const email = req.body.email;
   const password = md5(req.body.password);
 
-  //to check if the email was already used before
-  User.find((error,users)=>{
-    if(error){//if any errors occur
-      console.log(error);
-    }else{
-
-      //number of users having the same email
-      let usercount=0
-
-      //loops through all the users and check if the email that is given is used before
-
-      users.forEach((user)=>{//loops through all the users
-        if(user.email === email){//check if the email is used before
-          console.log(user);
-          res.send("user already exists");
-          usercount+=1;
-        }
-      })
-
-      if(usercount===0){//ie none fo the users match the email (new email used)
+  User.findOne({email:email},(err,user)=>{
+    if(err){
+      console.log(err)
+    } else{
+      if(user){
+        console.log(user)
+        res.send("User Exists!")
+      }else{
         const user = new User ({
           email:email,
           password:password
         })
         user.save()
-        res.send("saved!")
-      }
+        res.sendFile(__dirname+'/htmlFiles/secretsanta/start.html')
+
+              }
     }
   })
 });
+
+app.post('/login',(req,res)=>{
+  const email = req.body.email
+  const password = md5(req.body.password)
+
+  User.findOne({email:email},(err,user)=>{
+    if(err){
+      console.log(err)
+    } else{
+      if(user){
+        if(user.password === password){
+          console.log("user authenticated")
+          res.sendFile(__dirname+'/htmlFiles/secretsanta/start.html')
+        }else{
+          res.send("Incorrect password! Try again!")
+        }
+
+      }else{
+        res.send("User Doesn't Exist")
+      }
+    }
+  })
+
+
+
+})
+
 
 app.listen(3000,()=>{
   console.log("Server started at node 3000")
